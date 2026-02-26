b4f029399a9f09696e1d22b623dc2bce27b8cc9c
Jaymarc Ivan Tagulinao <ivantagulinao000@gmail.com>
2026-02-26 13:00:49 +0800

    Added reward points reflection in quotations and SO emails

---
+++ famtech_customizations_reward_points/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'FAMTECH Customizations',
+    'version': '1.0',
+    'summary': 'Custom modules for FAMTECH',
+    'category': 'Sales',
+    'depends': ['sale', 'mail'],
+    'data': [
+        'data/mail_templates_rewards.xml',
+        'views/sale_order_report.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
