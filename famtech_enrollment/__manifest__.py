fb02aeac43b517e8f3416cb599a132851018be5b
JomCasti <jomaricastillo8@gmail.com>
2026-02-27 14:17:25 +0800

    feat(data): add automated email templates and fix manifest routing

--- famtech_enrollment/__manifest__.py
+++ famtech_enrollment/__manifest__.py
@@ -5,11 +5,8 @@
   "depends": ["website", "website_sale", "website_slides", "payment", "calendar", "gamification", "survey", "website_coupon"],
   "data":[
     "data/mail_templates.xml",
-    "views/payment_templates.xml",
-    "views/course_templates.xml",
-    "views/booking_templates.xml",
+    "views/course_template.xml",
     "reports/certificate_template.xml",
-    "security/ir.model.access.csv"
   ],
   "installable": True,
 }
