b6036db42bd212a0be283459dd9bb8bb1a75d63f
JomCasti <jomaricastillo8@gmail.com>
2026-02-27 11:02:42 +0800

    chore(config): update manifest with new reports and views data

---
+++ famtech_enrollment/__manifest__.py
@@ -0,0 +1,15 @@
+{
+  "name": "FAMTECH Enrollment Automation",
+  "version": "1.0.0",
+  "category": "Website",
+  "depends": ["website", "website_sale", "website_slides", "payment", "calendar", "gamification", "survey", "website_coupon"],
+  "data":[
+    "data/mail_templates.xml",
+    "views/payment_templates.xml",
+    "views/course_templates.xml",
+    "views/booking_templates.xml",
+    "reports/certificate_template.xml",
+    "security/ir.model.access.csv"
+  ],
+  "installable": True,
+}
